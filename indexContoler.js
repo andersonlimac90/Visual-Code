3b9dd77cb18dd0abff278edb2cc3aa3afb4a5f1b
Anderson Costa <andersonlimac90@gmail.com>
2024-03-21 19:17:03 -0300

    erro visual code

---
+++ indexContoler.js
@@ -0,0 +1,26 @@
+class IndexContoler {
+    constructor(){
+        this.alunos = [];
+    }
+adicionarAluno(){
+    let id_aluno = this.alunos.length + 1 
+    let aluno = new Aluno(id_aluno,'Anderson','email@gmail.com')
+    this.alunos.push(aluno)
+    console.log(aluno)
+}
+mostrarTodos(){
+    for (let alu of this.alunos){
+        texto+= 'ID' + alu.id_aluno+' - ';
+        texto+= 'NOME' + alu.nome+' - ';
+        texto+= 'EMAIL' + alu.email+' - ';
+        document.getElementById('main').innerHTML = texto
+
+    }
+            
+}
+
+
+
+}
+
+var indexContoler = new IndexContoler();
